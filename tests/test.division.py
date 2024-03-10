24610c94a64c179004954cdd1bc8e2da62bb3878
Erick Peña <erick.pmartinez@hotmail.com>
2024-03-10 17:30:01 -0500

    d

--- tests/test.division.py
+++ tests/test.division.py
@@ -12,14 +12,21 @@ sys.path.append(str(root_path))
 from src.division import evaluar
 
 class TestDivision(unittest.TestCase):
-    def testDivisionExacta(self):
-        valor_esperado = "La división es exacta. \n" \
+    def testDivisionNoExacta(self):
+        valor_esperado = "La división no es exacta. \n" \
                          "Cociente: 2\n" \
                          "Residuo: 4"
         valor_actual = evaluar(14, 5)
         self.assertEqual(valor_esperado, valor_actual)
+
+        def testDivisionExacta(self):
+            valor_esperado = "La división  es exacta. \n" \
+                         "Cociente: 10\n" \
+                         "Residuo: 0"
+        valor_actual = evaluar(100, 10)
+        self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+   
     
 
 if __name__ == '__main__':
